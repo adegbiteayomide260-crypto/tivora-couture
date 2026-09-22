725e4571e86b4264da8e774446cd04fc336b8ebe
Ayomide <adegbiteayomide260@gmai.com>
2026-09-22 17:09:51 +0100

    Fix Firebase web config

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -9,7 +9,7 @@
    ========================================================================== */
 
 window.TIVORA_FIREBASE_CONFIG = {
-  apiKey: ''AIzaSyBd5UiqHXUOBvXroC81CEXADrId9ho-QC0,
+  apiKey: 'AIzaSyBd5UiqHXUOBvXroC81CEXADrId9ho-QC0',
   authDomain: 'tivoracouture.firebaseapp.com',
   projectId: 'tivoracouture',
   storageBucket: 'tivoracouture.firebasestorage.app',
